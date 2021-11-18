83aa0bcb059c74b980fb4b246d88f4368aa2a0d6
Bartosz Dokurno <bartusekcraft@gmail.com>
2021-11-18 17:49:19 +0100

    Better avatars handling

--- src/Event/Event.tsx
+++ src/Event/Event.tsx
@@ -23,7 +23,7 @@ export default class Event extends React.Component<IProps> {
         <div className="mt-3 d-flex">
           <div className="me-4 d-flex flex-column">
             <a href={actorURL(event.event.actor)}>
-              <Image height="80px" style={{minHeight: "80px"}} src={event.event.actor.avatar_url + "?size=80"} rounded />
+              <Image height="80px" style={{minHeight: "80px"}} src={this.avatarUrl(event) + "?size=80"} rounded />
             </a>
             <small className="text-center mt-1">5 minutes ago</small>
           </div>
@@ -33,5 +33,29 @@ export default class Event extends React.Component<IProps> {
         </div>
       </div>
     );
+  }
+
+  avatarUrl(eventInfo: IEventInfo) {
+    // event.actor.avatar_url doesn't contain valid avatar for bots for some reason
+    // this function gets their avatar from different sources 
+
+    const event = eventInfo.event
+    let url = ''
+    if(event.type === "PullRequestEvent") {
+      url = (event.payload as any).pull_request.user.avatar_url
+    }
+    else if(event.type === "IssuesEvent") {
+      url = (event.payload as any).issue.user.avatar_url
+    }
+    else if(event.type === "IssueCommentEvent" || event.type === "CommitCommentEvent") {
+      url = (event.payload as any).comment.user.avatar_url
+    }
+    else if(event.type === "ReleaseEvent") {
+      url = (event.payload as any).release.author.avatar_url
+    }
+    else {
+      url = event.actor.avatar_url
+    }
+    return url.replace("?v=4", "")
   }
 }
